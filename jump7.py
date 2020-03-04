fcea928cb08dca0f2095817b76b0441dc0a33924
Fabioooo <jellenjay@live.com>
2020-03-04 08:56:41 +0800

    homework1

---
+++ jump7.py
@@ -0,0 +1,9 @@
+git config --global user.name "Fabioooo"
+git config --global user.email "jellenjay@live.com"
+git commit -m "..."
+git remote add Repositoryname Repositorylink
+git push Repositoryname master
+
+# clone to gitHub
+cd [dir]
+git clone Repositorylink
